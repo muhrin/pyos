aad4a02315d2a0c82a8d9c8db9af2c53cef81698
Martin Uhrin <martin.uhrin@gmail.com>
2020-08-06 16:28:06 +0200

    Changed the pyos shell to load from entry point
    Commands can be incorporated into the pyos shell by defining a method
    that returns a list of CommandSets and exposing this via the
    pyos.plugins.shell entry point.

--- pyos/psh/shell.py
+++ pyos/psh/shell.py
@@ -1,20 +1,49 @@
+import logging
 import sys
+from typing import List
+
+import stevedore
 
 from . import base
 
-try:
-    from . import minki
-except ImportError:
-    minki = None
-
 __all__ = ('PyosShell',)
+
+PLUGINS_COMMANDS_NS = 'pyos.plugins.shell'
 
 
 class PyosShell(base.BaseShell):
     # pylint: disable=too-few-public-methods
 
-    if minki is not None:
-        do_workon = minki.do_workon
+    def __init__(self):
+        super().__init__()
+        for cmd_set in plugins_get_commands():
+            self.install_command_set(cmd_set)
+
+
+logger = logging.getLogger(__name__)
+
+
+def plugins_get_commands() -> List:
+    """Get all mincepy types and type helper instances registered as extensions"""
+    mgr = stevedore.extension.ExtensionManager(
+        namespace=PLUGINS_COMMANDS_NS,
+        invoke_on_load=False,
+    )
+
+    commands = []
+
+    def get_command(extension: stevedore.extension.Extension):
+        try:
+            commands.extend(extension.plugin())
+        except Exception:  # pylint: disable=broad-except
+            logger.exception("Failed to get command plugin from %s", extension.name)
+
+    try:
+        mgr.map(get_command)
+    except stevedore.extension.NoMatches:
+        pass
+
+    return commands
 
 
 if __name__ == '__main__':
