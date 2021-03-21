44ba3dce8602bedc9757a1e9f50f5e444557935a
Martin Uhrin <martin.uhrin@gmail.com>
2021-03-21 16:30:26 +0100

    Added test for find()ing using multipe paths

---
+++ test/fs/test_utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from mincepy import testing
+
+from pyos import psh
+from pyos import fs
+
+
+def test_find_multiple_paths():
+    """Test the find function when specifying multiple start points"""
+    ferrari = testing.Car(make='ferrari')
+    skoda = testing.Car(make='skoda')
+
+    psh.save(ferrari, 'path1/ferrari')
+    psh.save(skoda, 'path2/skoda')
+
+    assert len(fs.find(type=testing.Car)) == 2
+    assert len(fs.find('path1/')) == 1
+    assert len(fs.find('path1/', type=testing.Car)) == 1
+
+    # Multiple paths
+    assert len(fs.find('path1/', 'path2/')) == 2
+    assert len(fs.find('path1/', 'path2/', type=testing.Car)) == 2
